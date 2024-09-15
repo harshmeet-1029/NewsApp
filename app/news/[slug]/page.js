3166a130d2aaf94bbefc89ba461c206f621c1c6a
Harshmeet Singh <h.r8360389542@gmail.com>
2024-09-15 17:34:00 +0530

    INTERCEPTING ROUTES

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,4 +1,5 @@
 import { DUMMY_NEWS } from "@/dummy-news";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 const NewsDetailPage = ({ params }) => {
@@ -11,7 +12,10 @@ const NewsDetailPage = ({ params }) => {
   return (
     <article className="news-article">
       <header>
-        <img src={`/images/news/${newsItem?.image}`} alt={newsItem?.title} />
+        <Link href={`/news/${newsItem.slug}/image`}>
+          {" "}
+          <img src={`/images/news/${newsItem?.image}`} alt={newsItem?.title} />
+        </Link>
         <h1>{newsItem?.title}</h1>
         <time dateTime={newsItem?.date}>{newsItem?.date}</time>
       </header>
