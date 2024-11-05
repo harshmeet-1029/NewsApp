b7307865a2cd3504dbce1ad50630a14db178945d
Sunny Kumar <sunny@techformation.co.in>
2024-11-05 11:23:32 +0530

    Testing sub modules

---
+++ app/testing/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "Next.js Page Routing & Rendering",
+  description: "Learn how to route to different pages.",
+};
+
+const MarketingLayout = ({ children }) => {
+  return <main>{children}</main>;
+};
+
+export default MarketingLayout;
